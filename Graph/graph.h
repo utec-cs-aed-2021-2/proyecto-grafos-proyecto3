51bb6ca69c82345285d3fe7d27259390fdb46396
Jose Bojorquez <57500036+JoseBojorquez@users.noreply.github.com>
2021-11-18 11:33:22 -0500

    mi parte

--- Graph/graph.h
+++ Graph/graph.h
@@ -4,6 +4,7 @@
 #include <list>
 #include <vector>
 #include <unordered_map>
+#include <string>
 
 using namespace std;
 
@@ -33,24 +34,26 @@ struct Vertex {
 template<typename TV, typename TE>
 class Graph{
 private:    
-    std::unordered_map<string, Vertex<TV, TE>*>  vertexes;
+    unordered_map<string, Vertex<TV, TE>*>  vertexes;
     
 public:
-    bool insertVertex(string id, TV vertex) = 0;   
-    bool createEdge(string id1, string id2, TE w) = 0;     
-    bool deleteVertex(string id) = 0;     
-    bool deleteEdge(string id) = 0;   
-    TE &operator()(string start, string end)= 0;  
-    float density() = 0;
-    bool isDense(float threshold = 0.5) = 0;
-    bool isConnected()= 0;
-    bool isStronglyConnected() throw();
-    bool empty();
-    void clear()= 0;  
+    Graph(){}
+    ~Graph()=default;
+    virtual bool insertVertex(string id, TV vertex) = 0;   
+    virtual bool createEdge(string id1, string id2, TE w) = 0;     
+    virtual bool deleteVertex(string id) = 0;     
+    virtual bool deleteEdge(string start, string end) = 0;   
+    virtual TE &operator()(string start, string end)= 0;  
+    virtual float density() = 0;
+    virtual bool isDense(float threshold = 0.5) = 0;
+    virtual bool isConnected()= 0;
+    virtual bool isStronglyConnected();
+    virtual bool empty();
+    virtual void clear()= 0;  
       
-    void displayVertex(string id)= 0;
-    bool findById(string id) = 0;
-    void display() = 0;
+    virtual void displayVertex(string id)= 0;
+    virtual bool findById(string id) = 0;
+    virtual void display() = 0;
 };
 
 #endif
